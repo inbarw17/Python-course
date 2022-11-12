c9e244cd5956eeeb5e7c1e324650acfebf43d285
inbar <inbarw17@gmail.com>
2022-11-12 19:56:49 +0200

    python files

---
+++ python_demo/esacpe.py
@@ -0,0 +1,9 @@
+splitstring ="this string has been\n split over\t several \nlines"
+print(splitstring)
+
+
+tabbedstring = "1\t2\t3\t4\t5"
+print(tabbedstring)
+
+print('the petshop owner said "no, \'e\'s uh,... he\'s')
+
---
+++ python_demo/stings.py
@@ -0,0 +1,13 @@
+print("today is a nice day to learn python")
+print('python is fun')
+print("pyhton's string is easy to use")
+print ('we can eve include "quotes" in strings ')
+print("hello" + " world")
+greetings = "hello"
+name = "inbar"
+print (greetings+' ' +name)
+name = input ("please enter your name :" )
+account = "2342352535235"
+print (greetings+ ' ' + name + ' ' +account)
+
+
